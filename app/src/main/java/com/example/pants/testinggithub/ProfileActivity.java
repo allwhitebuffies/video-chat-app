0c8508c87a6984b0057a6bb11a1a0f347e6c5793
david kohn <kohn.20@osu.edu>
2015-04-13 18:45:52 -0400

    Navigation almost complete!
    Added navigation (and loading bar) between menu items.

--- app/src/main/java/com/example/pants/testinggithub/ProfileActivity.java
+++ app/src/main/java/com/example/pants/testinggithub/ProfileActivity.java
@@ -1,48 +1,41 @@
 package com.example.pants.testinggithub;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ProfileActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_profile, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_about:
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            case R.id.action_main:
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
                 startActivity(intent);
                 break;
-            case R.id.action_profile:
-                Intent intent2 = new Intent(MainActivity.this, ProfileActivity.class);
+            case R.id.action_about:
+                Intent intent2 = new Intent(ProfileActivity.this, AboutActivity.class);
                 startActivity(intent2);
                 break;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void clickShuffle(View view){
-        findViewById(R.id.shuffleButton).setVisibility(View.INVISIBLE);
-        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
     }
 }
