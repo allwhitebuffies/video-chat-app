a0a950824bc1117ec25272f90a19d62f6ba7a9b6
david kohn <kohn.20@osu.edu>
2015-04-14 15:23:43 -0400

    Revert "Revert "Navigation almost complete!""
    This reverts commit 925b51f10fcffecb6febb265595bb5e95188c475.

--- app/src/main/java/com/example/pants/testinggithub/AboutActivity.java
+++ app/src/main/java/com/example/pants/testinggithub/AboutActivity.java
@@ -1,48 +1,42 @@
 package com.example.pants.testinggithub;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class AboutActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_about, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
-            case R.id.action_about:
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            case R.id.action_main:
+                Intent intent = new Intent(AboutActivity.this, MainActivity.class);
                 startActivity(intent);
                 break;
             case R.id.action_profile:
-                Intent intent2 = new Intent(MainActivity.this, ProfileActivity.class);
+                Intent intent2 = new Intent(AboutActivity.this, ProfileActivity.class);
                 startActivity(intent2);
                 break;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void clickShuffle(View view){
-        findViewById(R.id.shuffleButton).setVisibility(View.INVISIBLE);
-        findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
     }
 }
