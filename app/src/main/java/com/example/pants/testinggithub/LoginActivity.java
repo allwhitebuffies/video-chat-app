8c4691a72e5b13801097ebb55b5766fc422703ad
david kohn <kohn.20@osu.edu>
2015-04-15 18:53:31 -0400

    attempt to add login functionality

---
+++ app/src/main/java/com/example/pants/testinggithub/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.example.pants.testinggithub;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+
+
+public class LoginActivity extends ActionBarActivity {
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_login);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
