f46d0ce7b50157035dd8a875be2a3bff86bf4e2f
david kohn <kohn.20@osu.edu>
2015-04-18 23:47:47 -0400

    facebook login working
    whoop there it is

--- app/src/main/java/com/example/pants/testinggithub/LoginActivity.java
+++ app/src/main/java/com/example/pants/testinggithub/LoginActivity.java
@@ -9,7 +9,11 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 
 public class LoginActivity extends ActionBarActivity {
@@ -21,8 +25,33 @@ public class LoginActivity extends ActionBarActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
+        LoginButton loginButton = (LoginButton) this.findViewById(R.id.login_button);
+        loginButton.setReadPermissions("public_profile","email");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                setContentView(R.layout.activity_main);
+                
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
